874813322c52ebcfd0b7286bd6a63dde3c524c16
samyr silva <ssamyrsilva.santana@gmail.com>
2025-03-19 11:41:26 +0000

    database done and starting doing the models

---
+++ src/main/java/com/bookvault/vault/model/Book.java
@@ -0,0 +1,28 @@
+package com.bookvault.vault.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "books")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String author;
+
+    private boolean available = true;
+}
+
